1a3bee34d50bd6f84bb5c1f96ff379e663ca87d0
Piotr Janiszewski <piotr.3.janiszewski@gmail.com>
2025-12-17 08:20:19 +0100

    u̱ _ u̱

--- test_setup/start.sh
+++ test_setup/start.sh
@@ -5,7 +5,7 @@ docker compose build
 docker compose up -d
 
 # Configurar impresora
-docker compose exec backend lpadmin -p MiImpresora -v socket://192.168.1.100:9100 -m everywhere
+#docker compose exec backend lpadmin -p MiImpresora -v socket://192.168.1.100:9100 -m everywhere
 
 # Acceder a CUPS web
 # http://localhost:631
